0f2c26591d9c5161fa783a30e2329faacd36605b
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-02-12 18:42:34 +0530

    Deref ajv schemas when building because ajv cannot process refs, add remaining routes

--- src/routes/guilds/guildsRouter.ts
+++ src/routes/guilds/guildsRouter.ts
@@ -34,6 +34,28 @@ export default (services: Services): Router => {
             res.status(500).send({ error: `${err}` });
         }
     });
+    
+    guildsRouter.get('/:guildId/templates', async (req, res) => {
+        try {
+            const data = await services.dbService.guild(req.params.guildId).templates();
+            res.send({ message: 'success', data: data });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.get('/:guildId/applications', async (req, res) => {
+        try {
+            const data = await services.dbService.guild(req.params.guildId).applications();
+            res.send({ message: 'success', data: data });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    /*****************************************************
+     * General settings routes
+     *****************************************************/
 
     const hasWhitespace = new RegExp(/\s/gm);
     guildsRouter.post('/:guildId/prefix/', validateBody(ajvSchema.SetPrefixPayload), async (req: ReqWithBody<GR.SetPrefixPayload>, res) => {
@@ -56,6 +78,10 @@ export default (services: Services): Router => {
         }
     });
 
+    /*****************************************************
+     * Punishment settings routes
+     *****************************************************/
+
     guildsRouter.post('/:guildId/punishments/add', validateBody(ajvSchema.AddPunishmentPayload), async (req: ReqWithBody<GR.AddPunishmentPayload>, res) => {
         try {
             const success = await services.dbService.guild(req.params.guildId).addPunishment(req.body);
@@ -76,6 +102,10 @@ export default (services: Services): Router => {
         }
     });
 
+    /*****************************************************
+     * Custom permit setting routes
+     *****************************************************/
+
     guildsRouter.post('/:guildId/permits/add', validateBody(ajvSchema.AddPermitPayload), async (req: ReqWithBody<GR.AddPermitPayload>, res) => {
         try {
             const success = await services.dbService.guild(req.params.guildId).addPermit(req.body);
@@ -89,6 +119,94 @@ export default (services: Services): Router => {
     guildsRouter.post('/:guildId/permits/remove', validateBody(ajvSchema.RemovePermitPayload), async (req: ReqWithBody<GR.RemovePermitPayload>, res) => {
         try {
             const success = await services.dbService.guild(req.params.guildId).removePermit(req.body.permitName);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/permits/permissions/', validateBody(ajvSchema.SetPermissionsPayload), async (req: ReqWithBody<GR.SetPermissionsPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setPermissions(req.body.permitName, req.body.permissions);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/permits/roles/', validateBody(ajvSchema.SetPermissionsPayload), async (req: ReqWithBody<GR.SetRolesPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setRoles(req.body.permitName, req.body.roles);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    /*****************************************************
+     * Advertisment warning settings routes
+     *****************************************************/
+
+    guildsRouter.post('/:guildId/adWarn/status/', validateBody(ajvSchema.SetAdwarnStatusPayload), async (req: ReqWithBody<GR.SetAdwarnStatusPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setAdwarnStatus(req.body.status);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/adWarn/channel/', validateBody(ajvSchema.SetAdwarnChannelPayload), async (req: ReqWithBody<GR.SetAdwarnChannelPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setAdwarnChannel(req.body.channel);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/adWarn/dmStatus/', validateBody(ajvSchema.SetAdwarnDmStatusPayload), async (req: ReqWithBody<GR.SetAdwarnDmStatusPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setAdwarnDmStatus(req.body.status);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/adWarn/message/', validateBody(ajvSchema.SetAdwarnMessagePayload), async (req: ReqWithBody<GR.SetAdwarnMessagePayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setAdwarnMessage(req.body.message);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    /*****************************************************
+     * Reports settings routes
+     *****************************************************/
+
+    guildsRouter.post('/:guildId/reports/status/', validateBody(ajvSchema.SetReportStatusPayload), async (req: ReqWithBody<GR.SetReportStatusPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setReportsStatus(req.body.status);
+            if(!success) res.status(500).send({ error: 'Database error.' });
+            else res.status(200).send({ message: 'Success' });
+        } catch(err) {
+            res.status(500).send({ error: `${err}` });
+        }
+    });
+
+    guildsRouter.post('/:guildId/reports/channel/', validateBody(ajvSchema.SetReportsChannelPayload), async (req: ReqWithBody<GR.SetReportsChannelPayload>, res) => {
+        try {
+            const success = await services.dbService.guild(req.params.guildId).setReportsChannel(req.body.channel);
             if(!success) res.status(500).send({ error: 'Database error.' });
             else res.status(200).send({ message: 'Success' });
         } catch(err) {
--- src/types/payloads/guildRoutes.ts
+++ src/types/payloads/guildRoutes.ts
@@ -1,7 +1,27 @@
-import { Permit, Punishment } from '../db';
+import { Message, Permit, Punishment } from '../db';
 
 export type SetPrefixPayload = { prefix: string };
+
 export type AddPunishmentPayload = Punishment;
 export type RemovePunishmentPayload = Punishment;
+
 export type AddPermitPayload = Permit;
 export type RemovePermitPayload = { permitName: string };
+
+export type SetPermissionsPayload = {
+    permitName: string
+    permissions: string[]
+};
+
+export type SetRolesPayload = {
+    permitName: string
+    roles: string[]
+};
+
+export type SetAdwarnStatusPayload = { status: boolean };
+export type SetAdwarnChannelPayload = { channel: string };
+export type SetAdwarnDmStatusPayload = { status: boolean };
+export type SetAdwarnMessagePayload = { message: Message };
+
+export type SetReportStatusPayload = { status: boolean };
+export type SetReportsChannelPayload = { channel: string };
